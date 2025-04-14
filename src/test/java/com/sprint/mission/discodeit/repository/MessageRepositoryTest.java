60f07b08ecbc2cbcbe5266c6b615915f1dd32fc0
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2025-04-14 19:34:32 +0900

    sprint8

--- src/test/java/com/sprint/mission/discodeit/repository/MessageRepositoryTest.java
+++ src/test/java/com/sprint/mission/discodeit/repository/MessageRepositoryTest.java
@@ -106,7 +106,7 @@ class MessageRepositoryTest {
     for (int i = 0; i < 3; i++) {
       Message message = messageRepository.save(new Message("메시지 " + i, channel, user, List.of()));
       saved.add(message);
-      Thread.sleep(10);
+      Thread.sleep(2000);
     }
 
     em.flush();
